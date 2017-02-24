7ac1fa0e34d4d8bb3875e0b25226b5b060901334
贾淼 <jam.jia@jiamiaodeMacBook-Pro.local>
2017-02-24 16:56:10 +0800

    add chapter1

---
+++ Chapter1/Chapter1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Chapter1
+//
+//  Created by 贾淼 on 2017/2/24.
+//  Copyright © 2017年 贾淼. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Enter two numbers:" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1+v2 << std::endl;
+    return 0;
+}
